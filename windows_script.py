70abfaa7999a7e2f07acf17e5e5e21de8283e8ce
Vishal Yadav <vishal.kumar@edneed.com>
2022-04-15 23:01:04 +0530

    changes in windows file

--- windows_script.py
+++ windows_script.py
@@ -18,3 +18,15 @@ print(f"Name: {my_system.Name}")
 print(f"NumberOfProcessors: {my_system.NumberOfProcessors}")
 print(f"SystemType: {my_system.SystemType}")
 print(f"SystemFamily: {my_system.SystemFamily}")
+
+# Initializing the wmi constructor
+f = wmi.WMI()
+ 
+# Printing the header for the later columns
+print("pid   Process name")
+ 
+# Iterating through all the running processes
+for process in f.Win32_Process():
+     
+    # Displaying the P_ID and P_Name of the process
+    print(f"{process.ProcessId:<10} {process.Name}")
