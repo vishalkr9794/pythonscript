1a2bab702f72a459360450b0a9ea0ed81e98b5f2
Vishal Yadav <vishal.kumar@edneed.com>
2022-04-16 13:14:21 +0530

    changes in linux script

---
+++ linux_script.py
@@ -0,0 +1,40 @@
+import platform
+
+# System Informations
+
+my_system = platform.uname()
+print(f"System: {my_system.system}")
+print(f"Node Name: {my_system.node}")
+print(f"Release: {my_system.release}")
+print(f"Version: {my_system.version}")
+print(f"Machine: {my_system.machine}")
+
+# processor
+print("Processors: ")
+with open("/proc/cpuinfo", "r")  as f:
+    info = f.readlines()
+
+cpuinfo = [x.strip().split(":")[1] for x in info if "model name"  in x]
+for index, item in enumerate(cpuinfo):
+    print("    " + str(index) + ": " + item)
+
+# Memory
+print("Memory Info: ")
+with open("/proc/meminfo", "r") as f:
+    lines = f.readlines()
+
+print("     " + lines[0].strip())
+print("     " + lines[1].strip())
+
+# uptime
+uptime = None
+with open("/proc/uptime", "r") as f:
+    uptime = f.read().split(" ")[0].strip()
+uptime = int(float(uptime))
+uptime_hours = uptime // 3600
+uptime_minutes = (uptime % 3600) // 60
+print("Uptime: " + str(uptime_hours) + ":" + str(uptime_minutes) + " hours")
+
+# Load
+with open("/proc/loadavg", "r") as f:
+    print("Average Load: " + f.read().strip())
